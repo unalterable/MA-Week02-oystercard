15d5918bd3778c8ed3bd0762c5be44ede6225e6c
paulkenrick <pkenrick@gmail.com>
2016-05-31 15:20:16 +0100

    Created a cap on the balance a card can have

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,15 @@ class Oystercard
 
   attr_reader :balance
 
+  MAXIMUM_BALANCE = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
     @balance += amount
+    raise "Balance can't exceed 90" if @balance > MAXIMUM_BALANCE
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,6 +4,8 @@ describe Oystercard do
 
   context '#top_up' do
 
+    let(:max_bal) { Oystercard::MAXIMUM_BALANCE }
+
     it 'checks new oystercard has zero balance' do
       expect(subject.balance).to eq(0)
     end
@@ -14,6 +16,11 @@ describe Oystercard do
 
     it 'adds money to oystercard' do
       expect{ subject.top_up(1) }.to change{ subject.balance }.by(1)
+    end
+
+    it 'does not allow balance to exceed maximum' do
+      message = "Balance can't exceed #{max_bal}"
+      expect{subject.top_up(max_bal + 1)}.to raise_error(message)
     end
 
   end
