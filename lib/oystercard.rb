a5ae24f7ccc243acd3806633d41505b0707d3118
Oliver Diamond <oliverdiamond@Olivers-MacBook-Pro-2.local>
2016-06-02 17:30:55 +0100

    Refactoring again

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,27 +4,28 @@ class Oystercard
 
   attr_reader :balance
   attr_reader :journeys
-
   MAXIMUM_BALANCE = 90
   MINIMUM_BALANCE = 1
+  BALANCE_ERRS = { max: "Balance can't exceed #{MAXIMUM_BALANCE}",
+                   min: "Balance can't fall below #{MINIMUM_BALACE}"}
+
+  
   def initialize
     @balance = 0
     @journeys = JourneyLog.build
   end
 
   def top_up(amount)
-    raise "Balance can't exceed 90" if amount + balance > MAXIMUM_BALANCE
+    raise BALANCE_ERRS[:max] if amount + balance > MAXIMUM_BALANCE
     @balance += amount
   end
 
   def touch_in(station_object)
-    raise "Already in a journey" if journeys.in_journey?
-    raise "Not enough funds" if balance < MINIMUM_BALANCE
+    raise BALANCE_ERRS[:min] if balance < MINIMUM_BALANCE
     journeys.begin(station_object)
   end
 
   def touch_out(station_object)
-    raise "Not yet started journey" unless journeys.in_journey?
     journeys.finish(station_object)
     deduct(journeys.log.last.fare)
   end
